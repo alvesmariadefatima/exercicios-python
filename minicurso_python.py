e238bdde7d033110c988d3022c0f37a6d6c460d5
Maria de Fátima Nunes Alves <94319702+mariadefatima1234@users.noreply.github.com>
2022-02-12 16:16:35 -0300

    Documentação do mini curso de Python Grupo Athena

---
+++ minicurso_python.py
@@ -0,0 +1,149 @@
+#print('Olá Mundo')
+
+#print(numero)
+
+#print(f'O número é igual a {numero}')
+
+#numero = input()
+
+#numero = input('Digite um número: ')
+
+#inteiro = int(input('Digite um número inteiro: '))
+
+#real = float(input('Digite um número real: '))
+
+#booleano = bool(input('Digite um valor: '))
+#numero = int(input('Digite um número: '))
+#if numero > 5:
+#print('O número é maior que 5.')
+#else:
+#print('O número não é maior que 5.')
+#elif numero==5:
+#print('O número é igual a 5.')
+#message = 'Número maior que 5' if numero > 5 else 'Número não é maior que 5.'
+#print(message)
+
+#numero = 1
+#while numero <= 5:
+#print(numero)
+
+#for x in [1,2,3,4,5]:
+#   print(x)
+
+#for x in range(5):
+#    print(x)
+
+#    for x in range(1,6):
+#        print(x)
+
+#        while 5>1:
+#            x=int(input('Digite 1 para sair: '))
+#            if x==1:
+#                break
+
+#minutos_por_processos = int(input())
+#processos = 8 * 60 / minutos_por_processos
+#print(processos)
+
+#tipos de strings
+
+#texto1 = 'Maria'
+#texto2 = "Fátima"
+#texto3 = '''Julia'''
+#texto4 = """Juliana"""
+
+#Manipulação de strings
+
+#frase = 'I love Python'
+#print(frase.upper())
+#print(frase.lower())
+
+#listas em python
+
+#lista1 = list('Grupo Athena')
+#print(lista1)
+
+#lista1 = list('Grupo Athena')
+#print(lista1)
+#lista1.sort()
+#print(lista1)
+
+#lista1 = list('Banana')
+#print(lista1.reverse())
+#print(lista1)
+
+#lista1 = list(range[1,11])
+#print(max(lista1))
+#print(min(lista1))
+
+#lista1 = list(range[1,11])
+#print(lista1.pop(4))
+#print(lista1)
+
+#lista2 = list(range[1,11])
+#lista2 = lista2.copy()
+
+#texto = 'Frase aleatória'
+#texto = texto.split('o')
+#print(texto)
+
+#tupla = tuple([1,2,3])
+#print(tupla)
+
+#tupla = 1,2,3, 'A','B', 'C'
+#print(tupla)
+
+#lista1 = [1,2,3]
+#print(lista1[1])
+
+#tupla1 = 1,2,3
+#print(tupla1[1])
+
+#tupla1 = 1,2,3
+#tupla1 = [1,2,3]
+#print(lista1)
+
+#lista1 = [1,2,3,3,4,5]
+#set1 = set(lista1)
+#print(set1)
+
+#set1 = {1,2,3}
+#set2 = {3,4,5}
+#print(set1 & set2) //and
+
+#lista1 = [1,2,3]
+#lista2 = [3,4,5]
+#lista3 = [lista1, *lista2]
+#print(lista3)
+
+#funções
+
+#def soma(x, y):
+
+#   return x + y
+
+#   else:
+#        return x
+
+#print(soma(10, 5))
+
+#def ola():
+#   print('Olá')
+
+#função lambda
+#soma = lambda x, y: x + y 
+#soma2 = lambda x, y: x + y
+
+#print(soma2(10,5))
+#ola()
+
+#lista = [1,2,3]
+#print(list(filter(lambda x: x % 2 == 0, lista)))
+
+
+
+
+
+
+
+
