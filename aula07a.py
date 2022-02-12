fa84e28d12709778af3956a7df551b934daffaec
Maria de Fátima Nunes Alves <94319702+mariadefatima1234@users.noreply.github.com>
2022-02-12 10:12:26 -0300

    Add files via upload

---
+++ aula07a.py
@@ -0,0 +1,10 @@
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+s = n1+n2
+d = n1-n2
+m = n1*n2
+div = n1//n2
+e = n1**n2
+
+print('A soma vale {}'.format(s, d, m, div, e))
+print('Divisão inteira {} e potência {}'.format(div, e))
