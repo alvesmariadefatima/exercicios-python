421fde369ee249b69208b6aa82dc6a56e6c8383c
Maria de Fátima Nunes Alves <94319702+mariadefatima1234@users.noreply.github.com>
2022-02-26 14:04:46 -0300

    Add files via upload

---
+++ aula008.py
@@ -0,0 +1,10 @@
+#from math import floor, sqrt, floor
+#num = int(input('Digite um número: '))
+#raiz = sqrt(num)
+#print('A raiz de {} é igual a {}'.format(num, floor(raiz)))
+
+#import random
+#num = random.randint(1,20)
+#print(num)
+
+
